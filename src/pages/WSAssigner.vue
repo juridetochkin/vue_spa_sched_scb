f93bbf69a8fda7a8eb67cd7f55f7ca915fa288f8
juridetochkin <detochkin.j@gmail.com>
2022-01-06 11:38:03 +0300

    feature qcalendar ext added

--- src/pages/WSAssigner.vue
+++ src/pages/WSAssigner.vue
@@ -1,21 +1,75 @@
 <template>
   <q-page class="q-pa-lg">
 
-    <h6 class="q-mt-none">Потребность фактическая</h6>
+    <h6 class="q-mt-none">Проставление смены</h6>
 
-    <div class="q-pa-md"></div>
+    <div class="q-pa-md">
+
+      <div class="row justify-center">
+        <div style="display: flex; max-width: 800px; width: 100%">
+          <q-calendar-resource
+            ref="calendar"
+            v-model="selectedDate"
+            v-model:model-resources="resources"
+            resource-key="id"
+            resource-label="name"
+            animated
+            bordered
+            @change="onChange"
+            @moved="onMoved"
+            @click-resource="onClickResource"
+            @click-head-resources="onClickHeadResources"
+            @click-interval="onClickInterval"
+          />
+        </div>
+      </div>
+
+    </div>
 
 
   </q-page>
 </template>
 
 <script>
-import {defineComponent} from 'vue';
+import { QCalendarResource, today } from '@quasar/quasar-ui-qcalendar/src/index.js'
+import '@quasar/quasar-ui-qcalendar/src/QCalendarVariables.sass'
+import '@quasar/quasar-ui-qcalendar/src/QCalendarTransitions.sass'
+import '@quasar/quasar-ui-qcalendar/src/QCalendarResource.sass'
+
+import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'WSAssigner',
-  setup(b) {
-    return {}
+  components: {
+    QCalendarResource
+  },
+  data () {
+    return {
+      selectedDate: today(),
+      resources: [
+        { id: '1', name: 'John' }
+      ]
+    }
+  },
+  methods: {
+    onPrev () {
+      this.$refs.calendar.prev()
+    },
+    onNext () {
+      this.$refs.calendar.next()
+    },
+    onMoved (data) {
+      console.log('onMoved', data)
+    },
+    onChange (data) {
+      console.log('onChange', data)
+    },
+    onClickResource (data) {
+      console.log('onClickResource', data)
+    },
+    onClickInterval (data) {
+      console.log('onClickInterval', data)
+    }
   }
 })
 </script>
